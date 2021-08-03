c47c2fb5ba70604621afb43454b057dc71e89f67
Resad Memmedov <talehg2932@gmail.com>
2021-08-03 11:58:00 +0400

    sncak games finish windows word

---
+++ Snake Games/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace Snake_Games
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnl = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.xal = new System.Windows.Forms.Label();
+            this.MaxXal = new System.Windows.Forms.Label();
+            this.SnakeMove = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.Color.Blue;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(6, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(876, 117);
+            this.label1.TabIndex = 0;
+            // 
+            // pnl
+            // 
+            this.pnl.BackColor = System.Drawing.Color.SlateGray;
+            this.pnl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnl.Font = new System.Drawing.Font("Bodoni MT Condensed", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.pnl.Location = new System.Drawing.Point(6, 71);
+            this.pnl.Name = "pnl";
+            this.pnl.Size = new System.Drawing.Size(876, 571);
+            this.pnl.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(58, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Toplam Xal :";
+            // 
+            // xal
+            // 
+            this.xal.AutoSize = true;
+            this.xal.BackColor = System.Drawing.Color.White;
+            this.xal.Location = new System.Drawing.Point(197, 23);
+            this.xal.Name = "xal";
+            this.xal.Size = new System.Drawing.Size(25, 26);
+            this.xal.TabIndex = 3;
+            this.xal.Text = "0";
+            // 
+            // MaxXal
+            // 
+            this.MaxXal.AutoSize = true;
+            this.MaxXal.BackColor = System.Drawing.Color.White;
+            this.MaxXal.Location = new System.Drawing.Point(832, 23);
+            this.MaxXal.Name = "MaxXal";
+            this.MaxXal.Size = new System.Drawing.Size(25, 26);
+            this.MaxXal.TabIndex = 5;
+            this.MaxXal.Text = "0";
+            // 
+            // SnakeMove
+            // 
+            this.SnakeMove.Interval = 150;
+            this.SnakeMove.Tick += new System.EventHandler(this.SnakeMove_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(651, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(175, 26);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "ən yaxşı nəticə :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(888, 645);
+            this.Controls.Add(this.MaxXal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.xal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pnl);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Snake Games";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label xal;
+        private System.Windows.Forms.Label MaxXal;
+        private System.Windows.Forms.Timer SnakeMove;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Snake Games/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake_Games
+{
+    
+    public partial class Form1 : Form
+    {
+        public int _snake_quantity;
+        private int _snake_size=20;
+        private Random _randomLocaton;
+        private Dircetion _dircetion;
+        private Label snakeHeat;
+        private Label mealSnake;
+        private int _snake_range=2;
+        List<int> resultList = new List<int>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+           _randomLocaton = new Random();
+            this.Hide();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Format();
+
+        }
+
+        private void Format()
+        {
+            _snake_quantity = 0;
+
+            CreateSnakeMeal();
+            SnakeLocation();
+            ChangeLocation();
+
+            SnakeMove.Enabled = true;
+            _dircetion = Dircetion.Right;
+        }
+
+        private Label CreateSnake(int locatinX,int locationY)
+        {
+            _snake_quantity++;
+            Label   CreateSnake = new Label()
+            {
+
+                Name = "snake" + _snake_quantity,
+                BackColor = Color.Red,
+                Width = _snake_size,
+                BorderStyle=BorderStyle.None,
+                Height=_snake_size,
+                Location=new Point(locatinX, locationY)
+                
+            };
+            pnl.Controls.Add(CreateSnake);
+            return CreateSnake;
+           
+
+
+
+        }
+        private void SnakeLocation()
+        {
+            snakeHeat = CreateSnake(0,0);
+            var locationX = (pnl.Width / 2)-(snakeHeat.Width/2);
+            var locationY = (pnl.Height / 2)-(snakeHeat.Height/2);
+            snakeHeat.Location = new Point(locationX, locationY);
+        }
+        private void CreateSnakeMeal()
+        {
+           
+            Label MealSnake = new Label()
+            {
+                Name = "meal" ,
+                BackColor = Color.Yellow,
+                Width = _snake_size,
+                BorderStyle = BorderStyle.FixedSingle,
+                Height = _snake_size,
+                
+
+            };
+          
+            pnl.Controls.Add(MealSnake);
+            mealSnake= MealSnake;
+
+        }
+        private void BeginAgain()
+        {
+            pnl.Controls.Clear();
+            xal.Text = "0";
+            Format();
+        }
+        private void ChangeLocation()
+        {
+            var locationX = _randomLocaton.Next(0,pnl.Width-_snake_size);
+            var locationY = _randomLocaton.Next(0,pnl.Height-_snake_size);
+            mealSnake.Location = new Point(locationX, locationY);
+        }
+        private enum Dircetion
+        {
+            Up,
+            Down,
+            Left,
+            Right
+
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            var keyCode = e.KeyCode;
+            if (_dircetion is Dircetion.Left&& keyCode is Keys.D
+                || _dircetion is Dircetion.Right && keyCode is Keys.A
+                || _dircetion is Dircetion.Up && keyCode is Keys.Z
+                || _dircetion is Dircetion.Down && keyCode is Keys.W
+                )
+            {
+                return;
+            }
+            switch (keyCode)
+            {
+                case Keys.W:
+                    _dircetion = Dircetion.Up;
+                    break;
+                case Keys.Z:
+                    _dircetion = Dircetion.Down;
+                    break;
+                case Keys.A:
+                    _dircetion = Dircetion.Left;
+                    break;
+                case Keys.D:
+                    _dircetion = Dircetion.Right;
+                    break;
+            }
+        }
+
+        private void SnakeMove_Tick(object sender, EventArgs e)
+        {
+
+
+            Snake_follow();
+            SnakeNorepeat();
+            FinishGames();
+            MealEat();
+           
+
+        }
+        private void SnakeNorepeat()
+        {
+            var _locationX = snakeHeat.Location.X;
+            var _locationY = snakeHeat.Location.Y;
+            switch (_dircetion)
+            {
+
+                case Dircetion.Up:
+                    snakeHeat.Location = new Point(_locationX, _locationY - (snakeHeat.Width + _snake_range));
+                    break;
+                case Dircetion.Down:
+                    snakeHeat.Location = new Point(_locationX, _locationY + (snakeHeat.Width + _snake_range));
+                    break;
+                case Dircetion.Left:
+                    snakeHeat.Location = new Point(_locationX - (snakeHeat.Width + _snake_range), _locationY);
+                    break;
+                case Dircetion.Right:
+                    snakeHeat.Location = new Point(_locationX + (snakeHeat.Width + _snake_range), _locationY);
+                    break;
+
+            }
+
+        }
+        public void MealEat()
+        {
+
+            var rect1 = new Rectangle(snakeHeat.Location, snakeHeat.Size);
+            var rect2 = new Rectangle(mealSnake.Location, mealSnake.Size);
+            if (rect1.IntersectsWith(rect2))
+            {
+
+                xal.Text = (int.Parse(xal.Text) + 10).ToString();
+                ChangeLocation();
+                CreateSnake(-_snake_size,-_snake_size);
+            }
+        }
+        private void Snake_follow()
+        {
+            if (_snake_quantity <= 1) return;
+
+
+            for (int i = _snake_quantity; i > 1; i--)
+            {
+                var nextPart = (Label)pnl.Controls[i];
+                var firstPart = (Label)pnl.Controls[i - 1];
+                nextPart.Location = firstPart.Location;
+            }
+        }
+        private void FinishGames()
+        {
+            bool _touch = false;
+            var rect1 = new Rectangle(snakeHeat.Location, snakeHeat.Size);
+            foreach(Control control in pnl.Controls)
+            {
+                if(control is Label &&control.Name.Contains("snake") && control.Name != snakeHeat.Name)
+                {
+                    var rect2 = new Rectangle(control.Location, control.Size);
+                    if (rect1.IntersectsWith(rect2))
+                    {
+                        _touch = true;
+                        resultList.Add(int.Parse(xal.Text));
+                        break;
+                    }
+                }
+            }
+            if (_touch)
+            {
+                SnakeMove.Enabled = false;
+                DialogResult result = MessageBox.Show($"Topladiginiz xal :{xal.Text}   yeniden baslamaq isterdiniz ?","Finis",MessageBoxButtons.YesNo,MessageBoxIcon.Stop);
+                if (result == DialogResult.Yes)
+                {
+                    BeginAgain();
+                    var _maxValue= resultList.Max();
+                    MaxXal.Text = $"{_maxValue.ToString()}";
+                    
+                }
+            }
+           
+        }
+    }
+}
